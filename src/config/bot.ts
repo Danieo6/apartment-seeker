02b019abd6f7edfa3eb970974eed6bba61668541
Danieo <daniel.budzinski0@gmail.com>
2021-08-05 14:08:48 +0200

    Feat: changed default fetch_interval

--- src/config/bot.ts
+++ src/config/bot.ts
@@ -1,4 +1,4 @@
 export const bot = {
   telegramToken: <string>process.env.TELEGRAM_BOT_TOKEN,
-  fetchInterval: +process.env.FETCH_INTERVAL! || 15000,
+  fetchInterval: +process.env.FETCH_INTERVAL! || 900000,
 };
