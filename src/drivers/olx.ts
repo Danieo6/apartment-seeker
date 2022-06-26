17539599291e8e41c5bd83e35addacfd76e963ea
Valentin Zayash <valentin.zayash@pandadoc.com>
2022-06-26 13:45:09 +0200

    fix: Fix localization field and imageUrl scraping

--- src/drivers/olx.ts
+++ src/drivers/olx.ts
@@ -9,8 +9,8 @@ export const olx = ($: cheerio.Root) => {
 
     if (offerUrl) {
       const title = $(container).find('div[type="list"] div h6').text()?.trim() || 'Title not found'
-      const imageUrl = $(container).find('div[class="css-pn1izb"] img').attr('src')?.split(';')[0] || 'http://via.placeholder.com/1200x800?text=Image%20not%20found';
-      const localization = $(container).find('p[data-testid="location-date"]').text()?.trim() || 'Not available';
+      const imageUrl = $(container).find('.css-gl6djm img').attr('srcset')?.split(';')[0] || 'http://via.placeholder.com/1200x800?text=Image%20not%20found';
+      const localization = $(container).find('p[data-testid="location-date"]').text()?.split(' - ')[0].trim() || 'Not available';
       const price = $(container).find('p[data-testid="ad-price"]').text()?.trim() || 'Not available';
       // Correct handling of the otodom.pl ads posted on olx.pl
       const fullUrl = /^(http)s?\/\//.test(offerUrl) ? offerUrl : 'https://www.olx.pl' + offerUrl;
