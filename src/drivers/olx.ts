bad0d7c2a7ffba661b324ae7b970089804f8ad9f
Valentin Zayash <valentin.zayash@pandadoc.com>
2022-06-27 23:51:48 +0200

    Revert "fix: Switch img attribute from the 'srcset' to 'src'"
    This reverts commit ce050ad45a52fa292355b8ab44f95fbaf4433130.

--- src/drivers/olx.ts
+++ src/drivers/olx.ts
@@ -11,7 +11,7 @@ export const olx = ($: cheerio.Root) => {
       const title = $(container).find('div[type="list"] div h6').text()?.trim() || 'Title not found'
       // TODO: Implement proper way to handle img/src for the OLX. 
       // Current approach sometimes resulted into wrong div/img or missing images
-      const imageUrl = $(container).find('.css-gl6djm img').attr('src')?.split(';')[0] || 'http://via.placeholder.com/1200x800?text=Image%20not%20found';
+      const imageUrl = $(container).find('.css-gl6djm img').attr('srcset')?.split(';')[0] || 'http://via.placeholder.com/1200x800?text=Image%20not%20found';
       const localization = $(container).find('p[data-testid="location-date"]').text()?.split(' - ')[0].trim() || 'Not available';
       const price = $(container).find('p[data-testid="ad-price"]').text()?.trim() || 'Not available';
       // Correct handling of the otodom.pl ads posted on olx.pl
